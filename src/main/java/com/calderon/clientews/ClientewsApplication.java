7c9fc52e86adf05b0c506b2ad8c3b4381242d225
Ivan Calderon <ivancs48@gmail.com>
2024-05-30 21:19:07 -0500

    git inicial

---
+++ src/main/java/com/calderon/clientews/ClientewsApplication.java
@@ -0,0 +1,13 @@
+package com.calderon.clientews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calderon/clientews/ClientewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calderon.clientews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
